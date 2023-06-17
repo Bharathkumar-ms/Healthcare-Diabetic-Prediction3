a38c816a5979720d5d1d706abe7ce770ca40199d
Bharathkumar M S <96257624+Bharathkumar-ms@users.noreply.github.com>
2023-06-17 16:04:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,28 @@ import numpy as np
 model = pickle.load(open('rf_healthcare.pkl', 'rb'))
 
 def welcome():
-    st.title('Welcome')
-    # Your code for the welcome page goes here
-    # ...
+    st.title(":green[Welcome to Homepage! 👋]")
+    st.header(":blue[Car Price Prediction :car]")
+    st.write('By: :red[Bharathkumar M S]')
+
+    st.write(" :green[For Prediction select page to 'Prediction' in side bar]")
+   
+        
+
+
+    btn_click = st.button("Click me to Know Who I am")
+
+    if btn_click == True:
+        st.subheader("You clicked me :cry:")
+        st.balloons()
+        st.title("I am :red[Bharathkumar M S]")
+        st.title("I am a  :violet[ Data Science Enthusiast]")
+        st.write("## Connect me on Linkedin [link](https://www.linkedin.com/in/bharathkumar-m-s-1736221b0/)")
+
+
 
 def submit():
-    st.title('Submit')
+    st.title('Prediction')
     pregnancies = st.number_input('Pregnancies', value=0, min_value=0, step=1)
     glucose = st.number_input('Glucose', value=0, min_value=0, step=1)
     bloodpressure = st.number_input('Blood Pressure', value=0, min_value=0, step=1)
@@ -20,7 +36,7 @@ def submit():
     bmi = st.number_input('BMI', value=0.0, min_value=0.0)
     age = st.number_input('Age', value=0, min_value=0, step=1)
 
-    if st.button('Submit'):
+    if st.button('Prediction'):
         result = model.predict([[pregnancies, glucose, bloodpressure, skinthickness, insulin, diabetes_pedigree_function, bmi, age]])
 
         if result == 0:
@@ -30,10 +46,10 @@ def submit():
 
 # Set up the app layout
 st.sidebar.title('Navigation')
-page = st.sidebar.selectbox('Select Page', ['Welcome', 'Submit'])
+page = st.sidebar.selectbox('Select Page', ['Welcome', 'Prediction'])
 
 # Render the selected page
 if page == 'Welcome':
     welcome()
-elif page == 'Submit':
+elif page == 'Prediction':
     submit()
